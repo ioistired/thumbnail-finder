1274061eaff801d1a10288e147735c57ee924095
bmintz <bmintz@users.noreply.github.com>
2017-11-25 23:48:24 -0600

    rest.py: make it runnable and log stuff

--- rest.py
+++ rest.py
@@ -1,14 +1,30 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import logging
+
 from flask import Flask, request, Response
 
 from thumbnail_finder import get_thumbnail_url
 
 app = Flask(__name__)
 
-API_VERSION = 0
+DEBUG = False
 
+API_VERSION = 0
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG if DEBUG else logging.WARNING)
 
 @app.route('/api/v{}/thumbnail'.format(API_VERSION))
 def main():
+	page_url = request.args.get('page_url')
+	logger.debug('Got page url ' + page_url)
 	image_url = get_thumbnail_url(request.args.get('page_url'))
 	result = image_url if image_url is not None else 'null'
+	logger.debug('Returning thumbnail url ' + result)
 	return Response(result, mimetype='text/plain')
+
+
+if __name__ == '__main__':
+	app.run(debug=DEBUG)
+	print('Running')
