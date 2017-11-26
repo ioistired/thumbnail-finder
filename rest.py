3f973b1082c3c5f179bb590f6c84e846ea691cb2
bmintz <bmintz@users.noreply.github.com>
2017-11-25 23:26:10 -0600

    fix the REST server

--- rest.py
+++ rest.py
@@ -1,13 +1,15 @@
-import thumbnail_finder
 from flask import Flask, request
+
+from thumbnail_finder import get_thumbnail_url
+
 app = Flask(__name__)
 
-@app.route('/')
+API_VERSION = 0
+
+
+@app.route('/api/v{}/thumbnail'.format(API_VERSION))
 def main():
-    url = request.args.get('url')
-    image = thumbnail_finder.get_thumbnail_url(url)
-    print(url)
-    print(image)
-    if not image == None:
-        return image
-    return "null"
+	image_url = get_thumbnail_url(request.args.get('page_url'))
+	if image_url is not None:
+		return image_url
+	return 'null'
