980ee2c9ea440c1224332048b3adbf6bcc45a0e9
bmintz <bmintz@users.noreply.github.com>
2017-11-26 00:32:07 -0600

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='thumbnail_finder',
-	version='0.2.1',
+	version='0.2.2',
 	url='https://github.com/bmintz/thumbnail-finder',
 
 	author='reddit Inc',
